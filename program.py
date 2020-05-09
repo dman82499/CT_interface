f53c14059906266b5ef5620b2bbdf5174e188f10
Devan Mallory <32146940+dman82499@users.noreply.github.com>
2020-05-09 03:25:28 -0400

    refactoring module names for organizational purposes

---
+++ program.py
@@ -0,0 +1,7 @@
+from PyQt5 import QtWidgets
+import PyQt5.QtWidgets.QApplication
+'''An interface for syncing the Cquential Application script with a robot, virutal or real, by Devan Mallory'''
+class main:
+    def __init__(self):
+        app = QApplication([])
+
